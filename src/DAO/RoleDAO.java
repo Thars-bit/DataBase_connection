dded7b4784c85b785d441e494387c7d49313e96a
Thars-bit <victordevdollar@gmail.com>
2025-07-19 22:55:37 -0500

    Module role DAO

--- src/DAO/RoleDAO.java
+++ src/DAO/RoleDAO.java
@@ -13,7 +13,7 @@ public class RoleDAO {
              PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
 
             statement.setString(1, role.getName());
-            statement.setString(1, role.getDescription());
+            statement.setString(2, role.getDescription());
 
             int affectedRows = statement.executeUpdate();
 
@@ -26,8 +26,33 @@ public class RoleDAO {
                 return true;
             }
         }catch (SQLException e){
-            System.err.println("Error al crear un usuario: " + e.getMessage());
+            System.err.println("Error al crear un rol: " + e.getMessage());
         }
         return false;
+    }
+
+    public List<Role> getAllRoles(){
+        List<Role> roles = new ArrayList<>();
+        String sql = "SELECT id, names, description FROM roles";
+
+        try (Connection connection = Connect_DB.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet result = statement.executeQuery(sql)) {
+
+            while (result.next()){
+                roles.add(new Role(
+                        result.getInt("id"),
+                        result.getString("names"),
+                        result.getString("description")
+                ));
+            }
+        }catch (SQLException e) {
+            System.err.println("Error al obtener los roles: "+ e.getMessage());
+        }
+        return roles;
+    }
+
+    public boolean updateRole(Role role){
+        String sql = "UPDATE roles SET "
     }
 }
