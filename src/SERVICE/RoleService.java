fbbb43b7fd5ddb2f366ae5f79ac0fa27f8c4024e
Thars-bit <victordevdollar@gmail.com>
2025-07-21 20:22:13 -0500

    roleService createRole complete

--- src/SERVICE/RoleService.java
+++ src/SERVICE/RoleService.java
@@ -14,5 +14,22 @@ public class RoleService {
         this.scanner = new Scanner(System.in);
     }
 
+    public void createRole(){
+        System.out.println("CREAR NUEVO ROL");
 
+        System.out.println("Ingrese el nombre del rol");
+        String names = scanner.nextLine();
+
+        System.out.println("Ingrese la descripcion");
+        String description = scanner.nextLine();
+
+
+        Role newRole = new Role(0, names, description);
+
+        if (roleDAO.createRole(newRole)){
+            System.out.println("Rol creado correctamente con ID: " + newRole.getId());
+        } else {
+            System.out.println("Error al crear el rol");
+        }
+    }
 }
