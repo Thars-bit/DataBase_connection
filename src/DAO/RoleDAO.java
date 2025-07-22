a67263b23099bd513ef5efbfa7b5e0fff54e40d1
Thars-bit <victordevdollar@gmail.com>
2025-07-21 20:13:01 -0500

    role DAO complete

--- src/DAO/RoleDAO.java
+++ src/DAO/RoleDAO.java
@@ -52,7 +52,55 @@ public class RoleDAO {
         return roles;
     }
 
+    public Role getRoleId(int id){
+        String sql = "SELECT id, names, description FROM roles WHERE id = ?";
+
+        try (Connection connection = Connect_DB.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setInt(1, id);
+            try(ResultSet result = statement.executeQuery()){
+                if(result.next()){
+                    return new Role(
+                            result.getInt("id"),
+                            result.getString("names"),
+                            result.getString("description")
+                    );
+                }
+            }
+        }catch (SQLException e){
+            System.err.println("Error al obtener el rol: "+ e.getMessage());
+        }
+        return null;
+    }
+
     public boolean updateRole(Role role){
-        String sql = "UPDATE roles SET "
+        String sql = "UPDATE roles SET names = ?, description = ? WHERE id = ?";
+
+        try (Connection connection = Connect_DB.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setString(1, role.getName());
+            statement.setString(2, role.getDescription());
+
+            return  statement.executeUpdate() > 0;
+        }catch (SQLException e){
+            System.err.println("Error al actualizar el rol: "+ e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean deleteRole(int id){
+        String sql = "DELETE FROM roles WHERE id = ?";
+
+        try (Connection connection = Connect_DB.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setInt(1, id);
+            return  statement.executeUpdate() > 0;
+        }catch (SQLException e){
+            System.err.println("Error al eliminar el rol: "+ e.getMessage());
+        }
+        return false;
     }
 }
--- src/SERVICE/RoleService.java
+++ src/SERVICE/RoleService.java
@@ -1,4 +1,18 @@
 package SERVICE;
 
+import DAO.RoleDAO;
+import MODEL.Role;
+import java.util.*;
+
 public class RoleService {
+
+    private RoleDAO roleDAO;
+    private Scanner scanner;
+
+    public  RoleService(){
+        this.roleDAO = new RoleDAO();
+        this.scanner = new Scanner(System.in);
+    }
+
+
 }
