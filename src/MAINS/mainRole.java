e83e16ef5e2e2b87298f27a6ebc15eaadf2bc1f7
Thars-bit <victordevdollar@gmail.com>
2025-07-22 20:45:00 -0500

    Module ROL complete

--- src/MAINS/mainRole.java
+++ src/MAINS/mainRole.java
@@ -1,4 +1,52 @@
 package MAINS;
 
+import SERVICE.RoleService;
+import java.util.*;
+
 public class mainRole {
+    public static void main(String[] args) {
+        RoleService roleService = new RoleService();
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit){
+            printMenu();
+            int option = Integer.parseInt(scanner.nextLine());
+
+            switch (option){
+                case 1:
+                    roleService.createRole();
+                    break;
+                case 2:
+                    roleService.listAllRoles();
+                    break;
+                case 3:
+                    roleService.updateRole();
+                    break;
+                case 4:
+                    roleService.deleteRole();
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        }
+
+        roleService.close();
+        scanner.close();
+        System.out.println("Gracias vemos socito");
+
+    }
+
+    private static void printMenu(){
+        System.out.println("\n--- SISTEMA DE GESTIÓN DE RROLES ---");
+        System.out.println("1. Crear rol");
+        System.out.println("2. Listar roles");
+        System.out.println("3. Actualizar rol");
+        System.out.println("4. Eliminar rol");
+        System.out.println("5. Salir");
+        System.out.print("Seleccione una opción: ");
+    }
 }
--- src/SERVICE/RoleService.java
+++ src/SERVICE/RoleService.java
@@ -32,4 +32,70 @@ public class RoleService {
             System.out.println("Error al crear el rol");
         }
     }
+
+    public void listAllRoles(){
+        System.out.println("Lista de roles");
+        List<Role> roles = roleDAO.getAllRoles();
+
+        if (roles.isEmpty()){
+            System.out.println("No hay roles en la Base de Datos");
+        }else {
+            roles.forEach(System.out::println);
+        }
+    }
+
+    public void updateRole(){
+        System.out.println("ACTUALIZACION DE ROLES");
+        listAllRoles();
+
+        System.out.println("Ingrese el ID del rol que quiere modificar");
+        int id = Integer.parseInt(scanner.nextLine());
+
+        Role role = roleDAO.getRoleId(id);
+        if (role == null){
+            System.out.println("Rol no encontrado");
+            return;
+        }
+
+        System.out.println("Nuevo nombre (" + role.getName() + "): ");
+        String newName = scanner.nextLine();
+        if (!newName.isEmpty()){
+            role.setName(newName);
+        }
+
+        System.out.println("Nueva descripcion (" + role.getDescription() + "): ");
+        String newDesgription = scanner.nextLine();
+        if (!newDesgription.isEmpty()){
+            role.setDescription(newDesgription);
+        }
+
+        if (roleDAO.updateRole(role)){
+            System.out.println("Rol actualizado correctamente");
+        }else {
+            System.out.println("error al alcualizar el rol");
+        }
+    }
+
+    public void deleteRole(){
+        System.out.println("ELIMINAR ROL");
+        listAllRoles();
+
+        System.out.println("Ingrese el ID del rol que quiere eliminar: ");
+        int id = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Esta seguro que quiere eliminar este rol? (s/n): ");
+        String confirm = scanner.nextLine();
+
+        if (confirm.equalsIgnoreCase("s")){
+            if (roleDAO.deleteRole(id)){
+                System.out.println("Rol eliminado correctamente");
+            }else {
+                System.out.println("Error al eliminar el rol pedazo de retrasado");
+            }
+        }
+    }
+
+    public void close(){
+        scanner.close();
+    }
 }
